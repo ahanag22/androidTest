cbc71446ce2dcb52568cbe89bc2b0d147cff97d5
Ahana Ghosh <ahanag22@uw.edu>
2017-04-11 09:06:46 -0700

    commit

--- app/src/main/java/tcss450/uw/edu/gittest/MainActivity.java
+++ app/src/main/java/tcss450/uw/edu/gittest/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //testing
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
